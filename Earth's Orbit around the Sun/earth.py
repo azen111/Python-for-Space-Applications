a4e3ec4e810ef4ef6c23457359edea03b8b53178
azen111 <adsingh.ca1@gmail.com>
2026-01-26 03:01:22 +0530

    Added plotting for the simulated data

--- Earth's Orbit around the Sun/earth.py
+++ Earth's Orbit around the Sun/earth.py
@@ -49,6 +49,9 @@ v =  np.empty(shape=(len(t),2))
 
 r[0], v[0] = r_0 , v_0 
 
+# choosing the numerical integration method
+method_integration ='euler'
+
 # defining the function that returns the accn vector when passed in the possitional vector
 def accn(r):
     return (-G*M_sun / np.linalg.norm(r)**3) * r
@@ -148,7 +151,7 @@ def rk4_method (r,v, accn, dt):
 
 
 
-def numerical_integration(r,v , accn, dt , method= 'euler'):
+def numerical_integration(r,v , accn, dt , method):
     """
     This function will aplly the numerical_integration based on the method choosen
     If the method is euler or rk4, the respective method will be implemented
@@ -170,22 +173,43 @@ def numerical_integration(r,v , accn, dt , method= 'euler'):
     else:
         raise Exception(f'You can either chhoose "euler" or "rk4". Your current input for method is:-{method}')
 
+numerical_integration(r,v,accn,dt, method=method_integration)
 
 # Find the point at which Earth is at its Aphelion
 sizes = np.array([np.linalg.norm(position) for position in r])
 pos_aphelion = np.max(sizes)
 arg_aphelion = np.argmax(sizes)
 vel_aphelion = np.linalg.norm(v[arg_aphelion])
-            
+
+
+
 
 # Plotting the simulated data on 3D axis
 plt.style.use('dark_background')
 plt.figure(figsize=(7,12))
+plt.subplot(projection='3d')
+suptitle_str = 'RK4' if method_integration.lower() == 'rk4' else 'Euler'
+plt.suptitle(suptitle_str + ' Method', color='r', fontsize=18, weight='bold')
 
-plt.plot(r[:,0], r[:,0], color= "tab:pink", lw = 2, label="orbit")
+plt.title(f'At Aphelion, the Earth is {round(pos_aphelion/1e9,1)} million km away from the Sun\nMoving at the speed of {round(vel_aphelion/1e3,1)} km/s.', fontsize=14, color='orange')
+plt.plot(r[:, 0], r[:,1], color= "tab:pink", lw = 2, label="orbit")
 plt.axis('off')
 plt.scatter(0,0, color = "yellow" , s = 1000, label ='Sun')
+plt.scatter(r[0,0], r[0,1], s=200, label='Earth at its Perihelion')
+plt.scatter(r[arg_aphelion,0], r[arg_aphelion,1], s=200, label='Earth at its Aphelion', color='blue')
+
+legend = plt.legend(loc="lower right", frameon =False)
+legend.legend_handles[1]._sizes =[150]
+legend.legend_handles[2]._sizes =[80]
+legend.legend_handles[3]._sizes =[80]
+
+
 plt.show()
+
+
+
+
+
 
 
 
