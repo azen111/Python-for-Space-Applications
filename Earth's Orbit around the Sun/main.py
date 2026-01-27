d12a49776afd506910f1b470c36f2cf625152b13
azen111 <adsingh.ca1@gmail.com>
2026-01-27 23:30:20 +0530

    restructured the project

--- Earth's Orbit around the Sun/main.py
+++ Earth's Orbit around the Sun/main.py
@@ -1,216 +1,47 @@
-'''
-Problem Statement
------------------
-- Create a simulation to track the orbit of the Earth around the Sun for a period of 1 year.
-- Use Euler and Runge - Kutta method of 4th order (RK4) for this task.
-- Find the distance from Earth to Sun at Apogee(farthest from the sun) using Euler and RK4 method and compare it with the original.
-
-Given Equations
----------------
-- Accn of Earth due to Gravity of the Sun
---> a = (-GM / |r|^3) * r_vec
-
-ODE for Position
---> dr/dt = v
-
-ODE for Velocity
---> dv/dt = a
-
-Initial Condition
------------------
---> Earth is at its Perihelion (closest to Sun)
-'''
-
-# imports
-import matplotlib.pyplot as plt
+# Imports
+from utils import read_json_config, accn, numerical_integration, at_aphelion, plot_simulated_data
 import numpy as np
 
-# Constraints
-G = 6.6743e-11
-M_sun =1.989e30
+def setup_simulation(config):
+    # Access configuration values
+    planet_name = config['planet_info']['name'] 
+    color_at_perihelion = config['planet_info']['perihelion_color']
+    color_at_aphelion = config['planet_info']['aphelion_color']
+    initial_position = np.array(config['initial_conditions']['position_at_perihelion'])*1e9 # m
+    initial_velocity = np.array(config['initial_conditions']['velocity_at_perihelion'])*1e3 # m/s
+    time_step = config['time_settings']['time_step'] # sec
+    max_time = config['time_settings']['simulation_time']*24*3600 # sec
+    method_integration = config['numerical_integration']['method'] 
 
-# initial Position and Velocitty
-r_0 = np.array([147.1e9, 0]) #meter #also for circular motion the position vector must ve prependicular to the velocity vector
-v_0 = np.array([0,-30.29e3]) #m/s  # earth in going to go around the sun in counter clockwise direction thus the negative sign
+    # Time array for numerical solution
+    t = np.arange(0, max_time, time_step)
 
-# time steps and total time for simulation
+    # Initialize arrays to store positions and velocities at all time steps
+    r = np.empty(shape=(len(t), 2))
+    v = np.empty(shape=(len(t), 2))
 
-dt = 3600 #seconds # i.e after how many seconds we are going to update our simulation
-t_max =3.154e7 #seconds (total seconds in 1 year)
+    # Set initial conditions for position and velocity
+    r[0], v[0] = np.array([initial_position, 0]), np.array([0, -initial_velocity])
 
-# time array to be used in numerical solution
-t = np.arange(0, t_max , dt)
+    return planet_name, color_at_perihelion, color_at_aphelion, r, v, t, time_step, method_integration
+ 
+# Read config.json
+config = read_json_config("config.json")
 
+# Constants
+G = 6.6743e-11    
+M_SUN = 1.989e30  # kg
 
-# initialize arrays to store positions and velocities at all time steps
-r =  np.empty(shape=(len(t),2))# this will create 2D array for r and v where len(t) is no. of time steps and 2 refers to x, y co-ordinates as r and v are vectors.
-v =  np.empty(shape=(len(t),2))
-# set the initial condition for position and velocity
+# Setup simulation
+planet_name, color_at_perihelion, color_at_aphelion, \
+      r, v, t, time_step, method_integration = setup_simulation(config)
 
-r[0], v[0] = r_0 , v_0 
+# Call numerical integration
+numerical_integration(G, M_SUN, r, v, accn, time_step, method=method_integration,)
 
-# choosing the numerical integration method
-method_integration ='euler'
+# Get data of Earth at its Aphelion
+arg_aphelion, vel_aphelion, pos_aphelion = at_aphelion(r, v)
 
-# defining the function that returns the accn vector when passed in the possitional vector
-def accn(r):
-    return (-G*M_sun / np.linalg.norm(r)**3) * r
-
-# Euler Integration
-
-def euler_method(r,v, accn, dt):
-    """
-    Equations for euler method
-    --------------------------
-    ODE for Position
-    --> dr/dt = v
-    --> r_new = r_old + v_old*dt
-
-    ODE for Velocity
-    --> dv/dt = a
-    --> v_new = v_old + a(r_old)*dt
-    parameters
-    ----------
-    r: empty array for possition of size t
-    v: empty array for velocity  of size t
-    a: func to calculate the accn at given possition
-    dt: time step for the simulation
-
-    this function will update the empty arrays for r and v with the simulated data
-    """
-
-    for i in range(1,len(t)):
-        r[i] = r[i-1] + v[i-1]*dt
-        v[i] = v[i-1] + accn(r[i-1])*dt
-
-# RK4 Integration
-
-def rk4_method (r,v, accn, dt):
-    '''
-    Equations for RK4 method
-    ------------------------
-    ODE for Position
-    --> dr/dt = v
-    --> r_new = r_old + dt/6(k1r + 2*k2r + 2k3r + k4r)
-
-    ODE for Velocity
-    --> dv/dt = a
-    --> v_new = v_old + dt/6(k1v + 2*k2v + 2k3v + k4v)
-
-    Methods to calculate the steps
-
-    ------------------------------
-
-    Step 1 :- 0
-    k1v = accn(r[i-1])
-    k1r = v[i-1]
-
-    Step 2 :- dt/2 using step 1
-    k2v = accn(r[i-1] + k1r * dt/2)
-    k2r = v[i-1] + k1v * dt/2
-
-    Step 3 :- dt/2 using step 2
-    k3v = accn(r[i-1] + k2r * dt/2)
-    k3r = v[i-1] + k2v * dt/2
-
-    Step 4 :- dt/2 using step 3
-    k4v = accn(r[i-1] + k3r * dt/2)
-    k4r = v[i-1] + k3v * dt/2
-
-    parameters
-    ----------
-    r: empty array for possition of size t
-    v: empty array for velocity  of size t
-    a: func to calculate the accn at given possition
-    dt: time step for the simulation
-
-    this function will update the empty arrays for r and v with the simulated data
-    '''
-
-    for i in range(1, len(r)):
-        # Step 1 :- 0
-        k1v = accn(r[i-1])
-        k1r = v[i-1]
-
-        # Step 2 :- dt/2 using step 1
-        k2v = accn(r[i-1] + k1r * dt/2)
-        k2r = v[i-1] + k1v * dt/2
-
-        # Step 3 :- dt/2 using step 2
-        k3v = accn(r[i-1] + k2r * dt/2)
-        k3r = v[i-1] + k2v * dt/2
-
-        # Step 4 :- dt/2 using step 3
-        k4v = accn(r[i-1] + k3r * dt/2)
-        k4r = v[i-1] + k3v * dt/2
-
-
-        # update the r and v
-        v[i] = v[i-1] + dt/6*(k1v + 2*k2v + 2*k3v + k4v)
-        r[i] = r[i-1] + dt/6*(k1r + 2*k2r + 2*k3r + k4r)
-
-
-
-def numerical_integration(r,v , accn, dt , method):
-    """
-    This function will aplly the numerical_integration based on the method choosen
-    If the method is euler or rk4, the respective method will be implemented
-    else it will raise an exception
-
-    parameters
-    ----------
-    r: empty array for possition of size t
-    v: empty array for velocity  of size t
-    a: func to calculate the accn at given possition
-    dt: time step for the simulation
-    method: either "euler" or "rk4" 
-
-    """
-    if method.lower() == 'euler':
-        euler_method(r,v,accn, dt)
-    elif method.lower() == 'rk4':
-        rk4_method(r,v, accn , dt)
-    else:
-        raise Exception(f'You can either chhoose "euler" or "rk4". Your current input for method is:-{method}')
-
-numerical_integration(r,v,accn,dt, method=method_integration)
-
-# Find the point at which Earth is at its Aphelion
-sizes = np.array([np.linalg.norm(position) for position in r])
-pos_aphelion = np.max(sizes)
-arg_aphelion = np.argmax(sizes)
-vel_aphelion = np.linalg.norm(v[arg_aphelion])
-
-
-
-
-# Plotting the simulated data on 3D axis
-plt.style.use('dark_background')
-plt.figure(figsize=(7,12))
-plt.subplot(projection='3d')
-suptitle_str = 'RK4' if method_integration.lower() == 'rk4' else 'Euler'
-plt.suptitle(suptitle_str + ' Method', color='r', fontsize=18, weight='bold')
-
-plt.title(f'At Aphelion, the Earth is {round(pos_aphelion/1e9,1)} million km away from the Sun\nMoving at the speed of {round(vel_aphelion/1e3,1)} km/s.', fontsize=14, color='orange')
-plt.plot(r[:, 0], r[:,1], color= "tab:pink", lw = 2, label="orbit")
-plt.axis('off')
-plt.scatter(0,0, color = "yellow" , s = 1000, label ='Sun')
-plt.scatter(r[0,0], r[0,1], s=200, label='Earth at its Perihelion')
-plt.scatter(r[arg_aphelion,0], r[arg_aphelion,1], s=200, label='Earth at its Aphelion', color='blue')
-
-legend = plt.legend(loc="lower right", frameon =False)
-legend.legend_handles[1]._sizes =[150]
-legend.legend_handles[2]._sizes =[80]
-legend.legend_handles[3]._sizes =[80]
-
-
-plt.show()
-
-
-
-
-
-
-
-
-
+# Plot the simulated data
+plot_simulated_data(r, method_integration, arg_aphelion, vel_aphelion, pos_aphelion, 
+                    planet_name, color_at_perihelion, color_at_aphelion)
